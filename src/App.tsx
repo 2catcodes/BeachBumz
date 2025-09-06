fa6b34ac7a00ff7209004e70ee83a43f99c23b2c
2catcodes <terrillcaden5@gmail.com>
2025-09-06 17:13:50 -0500

    site live live

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { HashRouter, Routes, Route, Navigate, useLocation } from "react-router-dom";
+import { Routes, Route, Navigate, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Menu from "./pages/Menu";
@@ -8,13 +8,15 @@ import "./index.css";
 
 function ScrollToTop() {
   const { pathname, search, hash } = useLocation();
-  useEffect(() => { window.scrollTo(0, 0); }, [pathname, search, hash]);
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname, search, hash]);
   return null;
 }
 
 export default function App() {
   return (
-    <HashRouter>
+    <>
       <ScrollToTop />
       <Navbar />
       <main className="pt-16 md:pt-20 min-h-screen">
@@ -25,6 +27,6 @@ export default function App() {
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </main>
-    </HashRouter>
+    </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,9 @@
-import { BrowserRouter } from 'react-router-dom';
-import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
-import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <BrowserRouter basename="/">
-<App />
-  </BrowserRouter>
-</React.StrictMode>
+createRoot(document.getElementById("root")!).render(
+  <HashRouter>
+    <App />
+  </HashRouter>
 );
